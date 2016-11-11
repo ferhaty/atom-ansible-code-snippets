e25413acd17b263d1a3e6484137350dbeceee5e1
Alex Nikankin <tsk.shad@gmail.com>
2016-11-11 17:22:28 +0500

    Add Modules: apt, apt_key, apt_repository, apt_rpm, yum, yum_repository, systemd

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1 - First Release
+* Modules: apt, apt_key, apt_repository, apt_rpm, yum, yum_repository, systemd
+
---
+++ spec/atom-ansible-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomAnsibleSnippetsView from '../lib/atom-ansible-snippets-view';
+
+describe('AtomAnsibleSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
